36ba8f2af32ecec58760c29334e01fe17fe08cc9
Fadil Rahadiansyah <rarayfront@gmail.com>
2021-05-26 02:29:24 +0700

    apply seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(AdminSeeder::class);
         $this->call(LaratrustSeeder::class);
+        $this->call(QuestionSeeder::class);
+        $this->call(ResultSeeder::class);
         
     }
 }
